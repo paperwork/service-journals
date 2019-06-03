a7ec493a23ce3f795505648a40250b7379e85e33
Marius <marius@twostairs.com>
2019-06-03 14:39:20 +0200

    application.ex

---
+++ lib/application.ex
@@ -0,0 +1,21 @@
+defmodule Paperwork.Journals.Application do
+    use Application
+
+    def start(_type, _args) do
+        case Code.ensure_loaded(ExSync) do
+            {:module, ExSync = mod} ->
+                mod.start()
+            {:error, _} ->
+                :ok
+        end
+
+        children = [
+            Paperwork.Ex,
+            Paperwork.Journals.Server,
+            {Mongo, [name: :mongo, url: Confex.fetch_env!(:paperwork, :mongodb)[:url], pool: DBConnection.Poolboy]}
+        ]
+
+        opts = [strategy: :one_for_one, name: Paperwork.Journals.Supervisor]
+        Supervisor.start_link(children, opts)
+    end
+end
