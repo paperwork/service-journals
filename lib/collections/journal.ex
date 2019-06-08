c9e45fca1c1aaee1638faa18762fadab8a10f0e9
Marius <marius@twostairs.com>
2019-06-08 12:23:46 +0200

    Implemented relevant_to, added query functions, added query for list

--- lib/collections/journal.ex
+++ lib/collections/journal.ex
@@ -13,6 +13,7 @@ defmodule Paperwork.Collections.Journal do
         resource: String.t(),
         resource_id: BSON.ObjectId.t() | nil,
         resource_system_id: String.t(),
+        relevant_to: List.t(),
         content: Map.t()
     }
     defstruct \
@@ -24,6 +25,7 @@ defmodule Paperwork.Collections.Journal do
         resource: "",
         resource_id: nil,
         resource_system_id: nil,
+        relevant_to: [],
         content: %{}
 
     use Paperwork.Collections
@@ -46,10 +48,59 @@ defmodule Paperwork.Collections.Journal do
         |> strip_privates
     end
 
+    @spec list(query :: Map.t) :: {:ok, [%__MODULE__{}]} | {:notfound, nil}
+    def list(%{} = query) when is_map(query) do
+        query
+        |> collection_find(true)
+        |> strip_privates
+    end
+
     @spec create(model :: __MODULE__.t) :: {:ok, %__MODULE__{}} | {:error, String.t}
     def create(%__MODULE__{} = model) do
         model
         |> collection_insert
         |> strip_privates
+    end
+
+    def query_resource(%{} = query, resource) when is_binary(resource) do
+        query
+        |> Map.put(
+            :resource,
+            resource
+        )
+    end
+
+    def query_resource(%{} = query, nil) do
+        query
+    end
+
+    def query_relevant_to(%{} = query, %Paperwork.Id{} = relevant_to) do
+        query
+        |> Map.put(
+            :relevant_to,
+            %{
+                id: relevant_to.id |> Paperwork.Id.maybe_id_to_objectid(),
+                system_id: relevant_to.system_id |> Paperwork.Id.maybe_id_to_objectid()
+            }
+        )
+    end
+
+    def query_relevant_to(%{} = query, nil) do
+        query
+    end
+
+    def query_newer_than_id(%{} = query, %Paperwork.Id{} = id) do
+        IO.inspect id
+        query
+        |> Map.put(
+            :_id,
+            %{
+                "$gt": id |> Paperwork.Id.to_objectid(:id)
+            }
+        )
+    end
+
+    def query_newer_than_id(%{} = query, nil) do
+        query
     end
 end
